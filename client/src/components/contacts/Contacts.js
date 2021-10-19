32f15e02057a56b35a7ad5bee8216c7207547dfd
lautp <lautaro.pretti@gmail.com>
2021-10-19 11:43:50 -0300

    gitingore fixed

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Contacts
